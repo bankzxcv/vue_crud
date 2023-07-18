aff7f06fb1f5ee31f516e14f587ea306416e119b
Jirayu <bankzxcv@gmail.com>
2023-07-18 02:23:00 -0700

    initial

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-app>
+    <v-main>
+      <AppBar />
+      <HelloWorld />
+    </v-main>
+  </v-app>
+</template>
+
+<script setup>
+  import HelloWorld from '@/components/HelloWorld.vue'
+  import AppBar from '@/components/AppBar.vue'
+</script>
---
+++ src/components/AppBar.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-app-bar :elevation="1">
+    <template v-slot:append>
+      <div class="">
+        <v-text-field label="Label" variant="outlined" class="w-auto pa-10"></v-text-field>
+      </div>
+      <v-btn icon="mdi-heart"></v-btn>
+      <v-btn icon="mdi-magnify"></v-btn>
+      <v-btn icon="mdi-dots-vertical"></v-btn>
+    </template>
+  </v-app-bar>
+</template>
+
+<style>
+</style>
